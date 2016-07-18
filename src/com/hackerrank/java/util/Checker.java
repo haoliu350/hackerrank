c3ea6bd31906cb36a5d41b67e76593022789e3df
Hao Liu <haoliu350@gmail.com>
2016-07-18 01:27:12 -0400

    Add utility class and package

---
+++ src/com/hackerrank/java/util/Checker.java
@@ -0,0 +1,14 @@
+package com.hackerrank.java.util;
+
+import java.util.Comparator;
+
+public class Checker implements Comparator<String>{
+
+	@Override
+	public int compare(String o1, String o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+}
